929160d005b7ae4e56d13b8266161b19cfed5144
Al1616 <rm3495@columbia.edu>
2019-02-26 13:56:41 -0500

    change0226

--- map.js
+++ map.js
@@ -1,1 +1,7 @@
-map.js
+'use strict'        // let the browser know we're serious
+
+// debug statement letting us know the file is loaded
+console.log('Loaded map.js')
+
+// your mapbox token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWwxNjE2IiwiYSI6ImNqc200ZXQ0YTBnOWE0NG54Ym45YnYybHgifQ.t9lM7oOjsxtKmQS_BGfbdg'
