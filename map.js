3483f151a86cd5c0067d7efb3c0af8e0b3af26ff
Al1616 <rm3495@columbia.edu>
2019-03-01 17:28:12 -0500

    five new place markers

--- map.js
+++ map.js
@@ -5,3 +5,108 @@ console.log('Loaded map.js')
 
 // your mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWwxNjE2IiwiYSI6ImNqc200ZXQ0YTBnOWE0NG54Ym45YnYybHgifQ.t9lM7oOjsxtKmQS_BGfbdg'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-73.96024, 40.80877],
+        zoom: 12
+})
+
+// create an instance of NavigationControl
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+
+// add the navigation to your map
+map.addControl(navigation, 'top-left')
+
+// create an instance of ScaleControl
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+
+// add the scale to your map
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+
+map.addControl(geolocate, 'top-left')
+
+// this is an event handler
+geolocate.on('geolocate', function(event) {
+
+    // create new variables to store the attributes we're interested in from the event
+    let lng = event.coords.longitude
+    let lat = event.coords.latitude
+
+    // debug
+    console.log('geolocated:', lng, lat)
+
+    // format lng lat values and display them on our 'info' element
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+map.on('click', function(event) {
+
+    let lng = event.lngLat.lng
+    let lat = event.lngLat.lat
+
+    console.log("clicked:", lng, lat)
+
+    document.getElementById('info').innerHTML = lng.toFixed(5) + "," + lat.toFixed(5)
+
+})
+
+let marker = new mapboxgl.Marker()
+marker.setLngLat([-73.96007,40.80871])
+marker.addTo(map)
+
+let popup = new mapboxgl.Popup()
+popup.setHTML('This is the Center for Spatial Research<br /><img src="https://currystonefoundation.org/wp-content/uploads/2018/05/csf_pr_csr_image5.jpg" />')
+marker.setPopup(popup)
+
+let data = [
+    {
+        location: [-74.01607,40.70182],
+        content: 'The furthest place I ran from school'
+    },
+    {
+        location: [-73.99411,40.70343],
+        content: 'Nice lobster roll'
+    },
+    {
+        location: [-73.99053,40.67611],
+        content: 'Play canoe in the canal'
+    },
+    {
+        location: [-73.97435,40.78138],
+        content: 'The longest corridor on the island'
+    },
+    {
+        location: [-73.96210,40.78615],
+        content: 'Largest Pond'
+    },
+    ]
+
+data.forEach(function(d) {
+
+    let marker = new mapboxgl.Marker()    
+    marker.setLngLat(d.location)
+    marker.addTo(map)  
+
+    let popup = new mapboxgl.Popup()
+    popup.setHTML(d.content)
+    marker.setPopup(popup)
+
+})
